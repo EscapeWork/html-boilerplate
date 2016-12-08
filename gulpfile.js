7489a9be0c7a4840eb954e001faf0b0bf712b4c9
Luis Dalmolin <luis.nh@gmail.com>
2016-12-08 11:46:27 -0200

    Changed to use laravel elixir and changed paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,38 @@
-const gulp       = require('gulp'),
-      notify     = require('gulp-notify'),
-      livereload = require('gulp-livereload')
-      changed    = require('gulp-changed'),
-      gutil      = require('gulp-util'),
-      compass    = require('gulp-compass'),
-      uglify     = require('gulp-uglify'),
-      concat     = require('gulp-concat'),
-      rename     = require('gulp-rename'),
-      source     = require('vinyl-source-stream'),
-      browserify = require('gulp-browserify'),
-      favicons   = require('gulp-favicons'),
-      timer      = null;
+const gulp        = require('gulp'),
+      elixir      = require('laravel-elixir')
+      gutil       = require('gulp-util'),
+      uglify      = require('gulp-uglify'),
+      favicons    = require('gulp-favicons'),
+      imagemin    = require('gulp-imagemin'),
+      spritesmith = require('gulp.spritesmith');
 
-var paths = {
-    scss: 'resources/assets/scss',
-    css: 'assets/css',
-    js: {
-        source: 'resources/assets/javascript',
-        dest: 'assets/js'
-    }
-};
+require('laravel-elixir-vue');
+require('laravel-elixir-livereload');
 
-gulp.task('sass', function() {
-    gulp.src(paths.scss + '/**/*.scss')
-        .pipe(changed(paths.css))
-        .pipe(compass({
-            config_file: 'config.rb',
-            style: 'compressed',
-            css: paths.css,
-            sass: paths.scss,
-            relative: false,
-            http_path: 'assets/images',
-            generated_images_path: 'assets/images/'
-        }))
-        .on('error', gutil.log)
-        .pipe(gulp.dest(paths.css));
+elixir.config.sourcemaps = false;
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(mix => {
+    mix.sass('main.scss', 'assets/css/main.css')
+       .webpack('app/main.js', 'assets/js/app/main.js');
+
+    mix.livereload();
 });
 
-gulp.task('uglify:vendor', function() {
-    gulp.src([
-            'resources/assets/components/jquery/dist/jquery.js',
-            'resources/assets/components/bootstrap-sass/assets/javascripts/bootstrap/modal.js'
-        ])
-        .pipe(uglify())
-        .pipe(concat('vendor.min.js'))
-        .pipe(gulp.dest('assets/vendor'));
-});
-
-gulp.task('scripts:app', function() {
-    gulp.src([paths.js.source + '/**/*.js', '!' + paths.js.source + '/modules/**'])
-        .pipe(browserify())
-        .on('error', gutil.log)
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('assets/js'));
-});
-
-// watching the files
-gulp.task('watch', function() {
-    livereload.listen();
-
-    gulp.watch(paths.scss      + '/**/*.scss', ['sass']);
-    gulp.watch(paths.js.source + '/**/*.js', ['scripts:app']);
-
-    gulp.watch([
-        paths.views   + '/**/*.php',
-        paths.css     + '/**/*.css',
-        paths.js.dest + '/**/*.js'
-    ]).on('change', reload);
-});
-
-gulp.task('reload', function() {
-    gulp.src('gulpfile.js')
-        .pipe(livereload());
+gulp.task('imagemin', function() {
+    gulp.src('assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('assets/images'));
 });
 
 gulp.task('favicons', function() {
@@ -89,21 +47,12 @@ gulp.task('favicons', function() {
         .pipe(gulp.dest('assets/images/icons'));
 });
 
-gulp.task('default', function() {
-    gulp.start('sass', 'scripts:app', 'watch');
+gulp.task('sprites', function() {
+    gulp.src('assets/images/sprites/default/*.png')
+        .pipe(spritesmith({
+            imgOutput: 'assets/images/sprites',
+            imgPath: '/assets/images/sprites/sprite.png',
+            cssName: '../../../../resources/assets/sass/components/_sprite.scss',
+        }))
+        .pipe(gulp.dest('assets/images/sprites'));
 });
-
-// actual reload function
-function reload() {
-    var reload_args = arguments;
-
-    // Stop timeout function to run livereload if this function is ran within the last 250ms
-    if (timer) clearTimeout(timer);
-
-    // Check if any gulp task is still running
-    if (!gulp.isRunning) {
-        timer = setTimeout(function() {
-            livereload.changed.apply(null, reload_args);
-        }, 250);
-    }
-}
---
+++ resources/assets/js/app/bootstrap.js
@@ -0,0 +1,22 @@
+// -------------------------------------------------------
+// jquery and bootstrap (and other vendors)
+window._ = require('lodash');
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass/assets/javascripts/bootstrap/modal.js');
+
+// require('jquery.inputmask/dist/jquery.inputmask.bundle.js');
+
+// -------------------------------------------------------
+// VueJS imports and config
+window.Vue = require('vue');
+require('vue-resource');
+
+Vue.http.interceptors.push((request, next) => {
+    request.headers['X-CSRF-TOKEN'] = $('meta[name="csrf-token"]').attr('content');
+    next();
+});
+
+// -------------------------------------------------------
+// Nossos modulos padrões
+// require('../modules/message');
+// require('../modules/contact');
