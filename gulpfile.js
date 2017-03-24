8d6c999fceca005ae1201ba43ecc24af8968c6c6
Luis Dalmolin <luis.nh@gmail.com>
2017-03-24 14:53:54 -0300

    Changed task builder to laravel mix

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,17 @@
 const gulp        = require('gulp'),
-      elixir      = require('laravel-elixir')
       gutil       = require('gulp-util'),
       uglify      = require('gulp-uglify'),
       favicons    = require('gulp-favicons'),
-      tingpng     = require('gulp-tinypng'),
-      spritesmith = require('gulp.spritesmith');
-
-require('laravel-elixir-vue');
-require('laravel-elixir-livereload');
-
-elixir.config.sourcemaps = false;
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(mix => {
-    mix.sass('main.scss', 'assets/css/main.css')
-       .webpack('app/main.js', 'assets/js/app/main.js');
-
-    mix.livereload();
-});
+      shell       = require('gulp-shell');
 
 gulp.task('imagemin', function() {
-    gulp.src(['assets/images/**/*.jpg', 'assets/images/**/*.png', '!assets/images/icons', '!assets/images/vendor'])
-        .pipe(tingpng('VYVWf4euo00lKqF6YczN9bx5lnPN1l1G'))
-        .pipe(gulp.dest('assets/images'));
+    gulp.src('public/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/assets/images'));
 });
 
 gulp.task('favicons', function() {
-    gulp.src('assets/images/icons/favicon.png')
+    gulp.src('public/assets/images/icons/favicon.png')
         .pipe(favicons({
             background: '#fff',
             path: '/assets/images/icons',
@@ -44,15 +19,17 @@ gulp.task('favicons', function() {
             replace: true
         }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('assets/images/icons'));
+        .pipe(gulp.dest('public/assets/images/icons'));
 });
 
-gulp.task('sprites', function() {
-    gulp.src('assets/images/sprites/default/*.png')
-        .pipe(spritesmith({
-            imgOutput: 'assets/images/sprites',
-            imgPath: '/assets/images/sprites/sprite.png',
-            cssName: '../../../../resources/assets/sass/components/_sprite.scss',
-        }))
-        .pipe(gulp.dest('assets/images/sprites'));
-});
+gulp.task('default', shell.task([
+    'npm run dev'
+]));
+
+gulp.task('watch', shell.task([
+    'npm run watch'
+]));
+
+gulp.task('production', shell.task([
+    'npm run production'
+]));
